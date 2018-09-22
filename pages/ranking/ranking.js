97c1f5b9ccbc1c972e3131bb3c19830a1654de31
luoxiao <360111767@qq.com>
2018-09-22 18:40:40 +0800

    update paihang

--- pages/ranking/ranking.js
+++ pages/ranking/ranking.js
@@ -1,7 +1,6 @@
 const app = getApp()
 var request = require("../../utils/request.js");
 var utils = require("../../utils/util.js");
-
 Page({
   data: {
     userId: '',    
@@ -10,137 +9,110 @@ Page({
     levelId: '', //用户的等级的ID
     levelList: [], //排行list
     myRanks: '',// 我的排名情况
-    levelType: 'score',//当前类型的排行
+    toView: 'to_',//跳到我的排名位置
+    scrollTop: 0,//滚动距离顶部的距离
+    levelType: 'world',//当前类型的排行
     listsHeight: '1000',//整个list的高度
     scrollHeight: '800',//滑动部分list排行榜的高度
+    lists_topbar: ['学位','词量']//世界排行 是名次 学位 词量；今日排行  名次 已背单词， 错题
   },
 
   onLoad: function () {
-    this.getWorldRank();
-    //this.loadFontFace();
+    //世界排行列表展示
+    this.getRankList();
     if (app.globalData.userInfo) {
       this.setData({
         userId: app.globalData.userId,
         userInfo: app.globalData.userInfo,
       })
     }
-    //utils.loadFont();
-    //utils.loadYouyuanFont();
-
   },
-  //获取连消排行
-  getScoreRank: function() {
-    //初始化levelList myRanks
-    this.setData({
-        levelList: {}, 
-        myRanks: ''
+  //获取排行列表
+  getRankList: function (event) {
+    let lists_topbar, API, levelType ;
+    //判断是否是onload调用，onload调用默认为'world'
+    if (event){
+      levelType = event.currentTarget.dataset.type;
+    }else{
+      levelType = 'world';
+    } 
+    //设置topbar
+    if(levelType == 'world'){
+      lists_topbar = ['学位', '词量'];
+      API = "RINKING_WORLD_LIST";
+    } else if (levelType == 'today'){
+      lists_topbar = ['已记单词'];
+      API = "TODAY_RINKING_LIST";
+    }else{
+      lists_topbar = ['学位', '词量'];
+      API = "";
+      this.setData({
+        levelList: [], //排行list
+        myRanks: '',// 我的排名情况
       });
-    // request.getData("RINKING_SCORE_LIST", { userId: app.globalData.userId })
-    // .then(res => {
-    //   let lists = res.list;
-    
-    //   this.setData({
-    //     levelList: lists, 
-    //     levelType: 'score',
-    //     levelNum: this.levelNum + lists
-    //   });
-    // })
-    // .catch(err => {
-    //   console.error("获取数据失败！")
-    //   console.log(err);
-    // })
-  },
-  //获取好友排行
-  getFriendsRank: function () {
+    }
     this.setData({
-      levelList: {},
-      myRanks: ''
+      lists_topbar,
+      levelType,
+      scrollTop: 0
     });
-    // request.getDataLoading("FRIENDS_SCORE_LIST", { userId: app.globalData.userId })
-    //   .then(res => {
-    //     let lists = res.list;
-      
-    //     this.setData({
-    //       levelList: lists,
-    //       levelType: 'friends',
-    //     });
-    //   })
-    //   .catch(err => {
-    //     console.error("获取数据失败！");
-    //     console.log(err);
-    //   })
+    let self = this;
+    if(API){
+      request.getDataLoading(API, { userId: app.globalData.userId }, '加载中...')
+        .then(res => {
+          let lists = res.rankList;
+          //最后一条是自己的排名
+          let myRanks;
+          let len = 0;
+          //中文解码，URL解码，nickname截取4个字符，
+          lists.forEach(function (item, index) {
+            item.nickname = decodeURI(item.nickname);
+            len = self.CheckChinese(item.nickname);
+            if ((item.nickname).length > len) {
+              item.nickname = (item.nickname).slice(0, len) + '..';
+            }
+            item.weiPic = decodeURIComponent(item.weiPic);
+            if (item.levelName) {
+              item.levelName = item.levelName.slice(0, 2);
+            } else {
+              item.levelName = '无尽' + item.levelMaxId;
+            }
+          });
+          myRanks = JSON.parse(JSON.stringify(lists[lists.length - 1]));
+          //删除最后的数据（自身的排名情况）
+          lists.pop();
+          this.setData({
+            levelList: lists,
+            myRanks
+          });
+        })
+        .catch(err => {
+          console.error("获取数据失败!");
+          console.log(err);
+        })
+    }
+    
+  }, 
+  //判断是否为中文
+  CheckChinese(str){
+    let Creg = new RegExp("[\\u4E00-\\u9FFF]","g"); 
+    let Ereg = new RegExp("[A-Za-z]","g");
+    if (str.length < 5 ) {
+      return str.length;
+    }
+    if (Creg.test(str)) {
+      return 3;
+    }else if (Ereg.test(str)){
+      return 6;
+    }else{
+      return 5;
+    }
   },
-  //获取世界排行
-  getWorldRank: function () {
-    //获取自己在世界的排行
-    this.getMyRank();
-    request.getData("RINKING_WORLD_LIST")
-      .then(res => {
-        let lists = res.worldRank;
-        //中文解码，URL解码，nickname截取6个字符，
-        lists.forEach(function(item,index){
-          item.nickname = decodeURI(item.nickname);
-          if((item.nickname).length>6){
-            item.nickname = (item.nickname).slice(0,6)+'..';
-          }
-          item.weiPic = decodeURIComponent(item.weiPic);
-          if (item.levelName){
-            item.levelName = item.levelName.slice(0, 2);
-          }else{
-            item.levelName = '';
-          }
-        });
-        this.setData({
-          levelList: lists
-        });
-      })
-      .catch(err => {
-        console.error("获取数据失败!");
-        console.log(err);
-      })
-  },
-  //获取我的排名情况
-  getMyRank: function () {
-    request.getData("MY_RINKING", { userId: app.globalData.userId})
-    .then(res => {
-          let item = res.myRank;
-          item.nickname = decodeURI(item.nickname);
-          if ((item.nickname).length > 6) {
-            item.nickname = (item.nickname).slice(0, 6) + '...';
-          }
-          item.weiPic = decodeURIComponent(item.weiPic);
-          if (item.levelName) {
-            item.levelName = item.levelName.slice(0, 2);
-          } else {
-            item.levelName = '';
-          }
-          
-        this.setData({
-          myRanks: JSON.parse(JSON.stringify(item))
-        });
-      })
-      .catch(err => {
-        console.error("获取数据失败!");
-        console.log(err);
-      })
-  },
-
-  //字体下载
-  // loadFontFace: function () {
-  //   const self = this
-  //   wx.loadFontFace({
-  //     family: this.data.fontFamily,
-  //     source: 'url("http://pfc6zcsy2.bkt.clouddn.com/幼圆.TTF")',
-  //     success(res) {
-  //       console.log(res.status)
-  //       self.setData({ loaded: true })
-  //     },
-  //     fail: function (res) {
-  //       console.log(res.status)
-  //     },
-  //     complete: function (res) {
-  //       console.log(res.status)
-  //     }
-  //   });
-  // },
+  //点击我自己，排行列表跳转到自己
+  jumpTo: function(){
+    this.setData({
+      toView: 'to_' + this.data.myRanks.myRank
+    });
+  }
+  
 })
